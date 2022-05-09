4a7d8369053cceb9782d9fd2c2d3a62609d359d5
Aamon Magnusson <aamonm6@gmail.com>
2022-05-08 20:47:29 -0600

    make it better

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 res=$(xrandr | grep "*" | awk '{print $1}' | awk -Fx '{print $2}')
 
-if [ "$res" == "768" ] || [ "$res" == "720" ];then
+if [ $(expr $res) < 1000 ];then
 	sed -i "s/pixelsize=18/pixelsize=10/g" config.h
 fi
 
 sudo make -s install clean
 
-if [ "$res" == "768" ] || [ "$res" == "720" ];then
+if [ $(expr $res) < 1000 ];then
 	sed -i "s/pixelsize=10/pixelsize=18/g" config.h
 fi
