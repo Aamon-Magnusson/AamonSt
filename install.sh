43d2d70020282d3ba1c6efbdcf470b5cc5ae7814
Aamon Magnusson <aamonm6@gmail.com>
2022-05-08 20:51:19 -0600

    lt

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 res=$(xrandr | grep "*" | awk '{print $1}' | awk -Fx '{print $2}')
 
-if [ $(expr $res) < 1000 ];then
+if [ $(expr $res) -lt 1000 ];then
 	sed -i "s/pixelsize=18/pixelsize=12/g" config.h
 fi
 
 sudo make -s install clean
 
-if [ $(expr $res) < 1000 ];then
+if [ $(expr $res) -lt 1000 ];then
 	sed -i "s/pixelsize=12/pixelsize=18/g" config.h
 fi
